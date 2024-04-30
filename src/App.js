a54b47f1cf2fa8dd6ee89159f5c473ce6793df61
MysticAakash07 <rahulsagar460@gmail.com>
2024-04-30 20:36:25 +0530

    Added add button functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import { MdOutlineDelete } from "react-icons/md";
 import { IoMdCheckmark } from "react-icons/io";
 function App() {
   const [isCompleteScreen,setIsCompleted] = useState(false);
+  const [allTodos,setTodos] = useState([]);
+  const [newTitle,setNewTitle] = useState("");
+  const [newDescription,setNewDescription] = useState("");
+
+  const handleAddTodo = () =>{
+    let newTodoItem = {
+      title:newTitle,
+      description:newDescription
+    }
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setTodos(updatedTodoArr);
+  }
   return (
     <div className="App">
       {/* Heading */}
@@ -16,16 +30,16 @@ function App() {
           {/* For taking the title Input */}
           <div className='todo-input-item'>
             <label for="Title">Title</label>
-            <input type="text" placeholder='Enter a Task ...' name='Title'/>
+            <input type="text" value={newTitle} onChange={(e)=>setNewTitle(e.target.value)} placeholder='Enter a Task ...' name='Title'/>
           </div>
           <div className='todo-input-item'>
             {/* For taking the Description Input */}
             <label for="Des">Description</label>
-            <input type="text" placeholder='Enter Description ...' name='Des'/>
+            <input type="text" value={newDescription} onChange={(e)=>setNewDescription(e.target.value)} placeholder='Enter Description ...' name='Des'/>
           </div>
             {/* For the button to add the task to the list */}
           <div className='todo-input-item'>
-            <button type='button' className='primaryBtn'>Add</button>
+            <button type='button' onClick={handleAddTodo} className='primaryBtn'>Add</button>
           </div>
         </div>
 
@@ -37,22 +51,29 @@ function App() {
 
         {/* To Do list items */}
         <div className='todo-list'>
-          <div className='todo-list-item'>
-            <h3>Task 1</h3>
-            <p>Description</p>
-            <div>
-            <MdOutlineDelete 
-              className='icon' 
-              // onClick={() => handleDeleteTodo (index) }
-              title="Delete?"
-            />
-            <IoMdCheckmark
-              className='check-icon icon' 
-              // onClick={() => handleComplete (index)}
-              title="Complete?"
-            />
-            </div>
-          </div>
+          
+            {allTodos.map((item,index) => {
+              return(
+                <div className='todo-list-item' key={index}>
+                  <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                  </div>
+                  <div>
+                  <MdOutlineDelete 
+                    className='icon' 
+                    // onClick={() => handleDeleteTodo (index) }
+                    title="Delete?"
+                  />
+                  <IoMdCheckmark
+                    className='check-icon icon' 
+                    // onClick={() => handleComplete (index)}
+                    title="Complete?"
+                  />
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
